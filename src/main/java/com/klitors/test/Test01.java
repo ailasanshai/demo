7cfbef629535ea29c8352767d454f54c936f0da7
爱辣三晒 <674446958@qq.com>
2016-12-25 22:36:02 +0800

    Initial commit

---
+++ src/main/java/com/klitors/test/Test01.java
@@ -0,0 +1,13 @@
+package com.klitors.test;
+
+/**
+ * Created by Administrator on 2016/12/25.
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+        Integer integer = new Integer(1);
+        System.out.println()
+                ;
+    }
+}
